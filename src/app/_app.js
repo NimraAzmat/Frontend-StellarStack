12f61f042f3524b43dd15aec1168f095e2d84460
Nimra Azmat <103200641+NimraAzmat@users.noreply.github.com>
2025-03-04 07:36:31 +0500

    Update _app.js

--- src/app/_app.js
+++ src/app/_app.js
@@ -1,5 +1,5 @@
-import Layout from "../components/Layout";
-import "../globals.css";
+import Layout from "@/app/components/Layout";
+import "@/app/globals.css";
 
 function MyApp({ Component, pageProps }) {
   return (
